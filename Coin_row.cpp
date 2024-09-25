e978a95c9b2f01aebc912ac9aeb6d3fd8f7b4471
Mehbinamal <mehbinamal6@gmail.com>
2024-09-25 15:16:06 +0530

    Coin Row Problem

---
+++ Coin_row.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void coin_row (int C[],int n){
+    int F[n],B[n];
+    F[0] = 0;
+    F[1] = C[1];
+    B[0] = 0;
+    B[1] = 1;
+    for(int i =2;i<n;i++){
+        if(C[i]+F[i-2]>F[i-1]){
+            B[i] = i-2;
+            F[i] = C[i]+F[i-2];
+        }
+        else{
+            B[i] = i-1;
+            F[i] = F[i-1];
+        }
+        F[i] = max(C[i]+F[i-2],F[i-1]);
+    }
+    cout<<"Max Value :"<<F[n-1]<<endl;
+    cout<<"Chosen Coins: ";
+    for(int i=n;i>=0;){
+        if(B[i] == i-2){
+            cout<<C[i]<<" ";
+            i=i-2;
+        }
+        else if(i==1){
+            cout<<C[i];
+            i = i-1;
+        }
+        else{
+            i--;
+        }
+    }
+    cout<<endl;
+}
+
+int main(){
+    int n;
+    cout<<"Enter number Of Coins:";
+    cin>>n;
+    int A[n];
+    cout<<"Enter Coins :";
+    A[0] = 0;
+    for(int i=1;i<=n;i++){
+        cin>>A[i];
+    }
+    coin_row(A,n+1);
+    return 0;
+}
